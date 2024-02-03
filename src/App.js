4b8611229fb1082ce90b1e44fc5ea70d277719fd
Алексей Горощеня <lastpaladin13@gmail.com>
2024-02-03 12:17:15 +0300

    added wallet modal

--- src/App.js
+++ src/App.js
@@ -4,20 +4,43 @@ import './App.css';
 
 import Main from './components/main/Main';
 
+import { createWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi/react'
+
+import { WagmiConfig } from 'wagmi'
+import { arbitrum, mainnet } from 'viem/chains'
+
+//more about libriry https://docs.walletconnect.com/web3modal/react/about?platform=wagmi1
+// 1. Get projectId at https://cloud.walletconnect.com
+const projectId = '4bf35fbc963cd2af20592850dd871cb7'
+
+
+const metadata = {
+  name: 'Web3Modal',
+  description: 'Web3Modal Example',
+  url: 'https://web3modal.com',
+  icons: ['https://avatars.githubusercontent.com/u/37784886']
+}
+
+const chains = [mainnet, arbitrum]
+const wagmiConfig = defaultWagmiConfig({
+  chains,
+  projectId,
+  metadata,
+  enableAnalytics: true // Optional - defaults to your Cloud configuration
+})
+
+
+createWeb3Modal({ wagmiConfig, projectId, chains })
+
 
 function App() {
 
-
-
-
-  return (
-    <div className="App"
-    >
+  return <WagmiConfig config={wagmiConfig}>
+    <div className="App">
       <Main/>
-      
-
     </div>
-  );
+  </WagmiConfig>
+
 }
 
 export default App;
