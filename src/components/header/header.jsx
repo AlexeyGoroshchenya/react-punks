2788a66b9495c852c86c4edf1465bd8381da304e
Алексей Горощеня <lastpaladin13@gmail.com>
2024-02-05 18:58:54 +0300

    add render accout in button after conection

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './header.module.css'
 import { useWeb3Modal } from '@web3modal/wagmi/react'
+import { useAccount } from 'wagmi'
 
 const Header = ({stage}) => {
 
     const { open } = useWeb3Modal()
+    const { address, isConnecting, isDisconnected } = useAccount()
+    const [accountShortName, setAccountShortName] = useState('')
+
+const getAccountShortName = (str)=>{
+    return str.slice(0,3)+'...'+str.slice(35)
+
+}
+
+    useEffect(()=>{
+        if(address) setAccountShortName(getAccountShortName(address))
+        
+    },[address])
+
+
+    
 
     return (
-        <div className={styles.body}>
+        <div className={address?styles.body + ' ' + styles.connected: styles.body}>
             <div className={styles.container}>
                 <div className={stage===1?'coral ' + styles.logo:stage===2?'green ' + styles.logo:'purple ' + styles.logo}>BLASTPUNKS</div>
                 <div className={styles.contacts}>
@@ -20,7 +36,7 @@ const Header = ({stage}) => {
                 onClick={() => open()}
                 className={stage===1?styles.button_1 +' ' + styles.button:stage===2?styles.button_2 +' ' + styles.button: styles.button_3 +' ' + styles.button}>
                     
-                    <p >connect</p>
+                    <p >{address? accountShortName: 'connect'}</p>
                 </div>
             </div>
         </div>
